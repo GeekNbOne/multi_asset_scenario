7bf54fa4ecfc4bae7abd00f40b4c81c9ea1f370e
LucBerthiaume <berthiaume.luc@gmail.com>
2018-08-11 20:26:40 -0400

    Initial Commit

---
+++ Multi_Asset_Scenario/main.cpp
@@ -0,0 +1,20 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Aug 10, 2018
+ *      Author: LucBerthiaume
+ */
+
+
+#include <iostream>
+
+
+using namespace std;
+
+int main(void){
+
+	cout << "Hello World" << endl;
+
+	return 0;
+}
+
